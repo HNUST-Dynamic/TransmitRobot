4152fa85cde88ded9e99b6b517092219e96da6b2
noneofever <2651984573@qq.com>
2024-08-18 23:32:36 +0800

    modify at 8.18-night

--- Modules/Algorithm/DataFrame.c
+++ Modules/Algorithm/DataFrame.c
@@ -31,13 +31,13 @@ StepMotorDataFrame_s GenerateDataFrame(StepMotorInstance *motor_instance)
     frame.subdivision = motor_instance->subdivision;
 
     // 将数据值和转速值转换为高低八位
-    frame.data_high = (motor_instance->data_value * 10) >> 8;
-    frame.data_low = (motor_instance->data_value * 10) & 0xFF;
-    frame.speed_high = (motor_instance->speed_value * 10) >> 8;
-    frame.speed_low = (motor_instance->speed_value * 10) & 0xFF;
+    frame.data_high = (motor_instance->data * 10) >> 8;
+    frame.data_low = (motor_instance->data * 10) & 0xFF;
+    frame.speed_high = (motor_instance->speed * 10) >> 8;
+    frame.speed_low = (motor_instance->speed * 10) & 0xFF;
 
     // 计算 BCC 校验位
-    frame.bcc_checksum = CalculateRcc(&frame);
+    frame.rcc_checksum = CalculateRcc(&frame);
 
     // 设置固定的帧尾
     frame.frame_tail = 0x7D;
