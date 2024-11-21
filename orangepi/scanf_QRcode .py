596f67f834e32a64b66201882f25f7da2a343093
2603683633q <168770349+2603683633q@users.noreply.github.com>
2024-11-21 16:42:54 +0800

    Update scanf_QRcode .py

--- orangepi/scanf_QRcode .py
+++ orangepi/scanf_QRcode .py
@@ -6,7 +6,7 @@ import time
 import sys  # 导入系统模块，用于退出程序
 
 # 定义帧头和帧尾
-FRAME_HEADER = b'\x02'  # 十六进制表示帧头 (STX)
+FRAME_HEADER = b'\x01'  # 十六进制表示帧头 (STX)
 FRAME_TAIL = b'\x03'    # 十六进制表示帧尾 (ETX)
 
 def decode_qr_code(image):
@@ -23,35 +23,44 @@ def decode_qr_code(image):
         print(f"QR Code Data: {qr_data}")
 
         # 加上帧头帧尾并发送二维码数据
-        data_with_frame = FRAME_HEADER + f"{qr_data}\n".encode('utf-8') + FRAME_TAIL
-        ser.write(data_with_frame)
+        data_with_frame1 = FRAME_HEADER + f"{qr_data}".encode('utf-8') + FRAME_TAIL
+        data_with_frame2 = f't0.txt="{qr_data}"' 
+        data_with_frame3=b'\xff\xff\xff'
+        # 发送第一个数据
 
+        ser1.write(data_with_frame1)
+          
+        print("发送第一个数据:", data_with_frame1)
+        time.sleep(0.01)  # 添加延时，确保数据完整发送
+
+        # 发送第二个数据
+        ser2.write(data_with_frame3)
+        time.sleep(0.01)
+        ser2.write(data_with_frame2.encode('utf-8'))
+        time.sleep(0.01)
+        ser2.write(data_with_frame3)
+        print("发送第二个数据:", data_with_frame2)
         return True 
 
     return False
 
 # 初始化串口
-ser = serial.Serial('/dev/ttyS1', 9600, timeout=1)
-time.sleep(2)
+ser1 = serial.Serial('/dev/ttyS1', 115200, timeout=1)  # 第一个串口
+ser2 = serial.Serial('/dev/ttyS3', 115200, timeout=1)  # 第2个串口
+
+time.sleep(0.01)
 
 # 初始化摄像头
-camera = cv2.VideoCapture('/dev/came1')
+camera = cv2.VideoCapture(2)
 
 if not camera.isOpened():
     raise IOError("无法打开摄像头")
-
+#ser.write(FRAME_HEADER + b'0\n' + FRAME_TAIL)
+#print("摄像头已打开，发送 '0' 到串口")
 try:
-    print("等待串口接收到 '1' 指令...")
     
     while True:
         # 检查串口是否有数据传入
-        if ser.in_waiting > 0:
-            request = ser.readline().decode('utf-8').strip()
-            print(f"Received request: {request}")
-
-            # 当收到的指令是 '1' 时开始扫描二维码
-            if request == "1":
-                print("收到 '1' 指令，开始扫描二维码...")
                 while True:
                     ret, frame = camera.read()
                     if not ret:
@@ -70,11 +79,10 @@ try:
                     if cv2.waitKey(1) & 0xFF == ord('q'):
                         break
 
-        # 按 'q' 键退出程序
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+
 
 finally:
     camera.release()
-    ser.close()
+    ser1.close()
+    ser2.close()
     cv2.destroyAllWindows()
